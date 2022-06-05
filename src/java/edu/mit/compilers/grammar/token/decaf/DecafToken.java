df16a8b82b5d7be0c295200ee43f77f4de55075c
Anarion-zuo <anarionzuo@outlook.com>
2022-06-05 13:48:45 +0800

    special char

--- src/java/edu/mit/compilers/grammar/token/decaf/DecafToken.java
+++ src/java/edu/mit/compilers/grammar/token/decaf/DecafToken.java
@@ -1,5 +1,6 @@
 package edu.mit.compilers.grammar.token.decaf;
 
+import com.google.common.base.Utf8;
 import edu.mit.compilers.grammar.token.Token;
 
 import java.util.HashSet;
@@ -8,7 +9,7 @@ import java.util.List;
 public class DecafToken extends Token {
     private static final HashSet<String> reservedWords = new HashSet<>(List.of(
         "bool", "break", "import", "continue", "else", "false", "for",
-            "while", "if", "int", "return", "len", "true", "void"
+            "while", "if", "int", "return", "len", "true", "void", "boolean", "callout", "class"
     ));
 
     protected static char escapeCharacter(char ch) {
@@ -57,5 +58,12 @@ public class DecafToken extends Token {
 
     public static boolean isReservedWord(String word) {
         return reservedWords.contains(word);
+    }
+
+    protected static boolean isUtf8Viewable(char ch) {
+        return !(
+                (ch >= 0 && ch <= 0x1f) ||
+                        (ch >= 0x7f && ch <= 0x9f)
+                );
     }
 }
